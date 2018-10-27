ccffd67dabcb22ea1cc1ef1c9bf7fa61df493052
Aayush Dutt <aayushd100@gmail.com>
2018-10-27 18:31:27 +0530

    add post route in /medData

--- server/routes/medData.js
+++ server/routes/medData.js
@@ -16,14 +16,15 @@ router.get('/', function(req, res, next) {
 });
 
 /* POST med data. */
-// router.post('/', function(req, res, next) {
-//   db.MedData.create(req.body)
-//       .then(function(newMedData) {
-//           res.json({newMedData})
-//       })
-//       .catch(function(err) {
-//           res.send(err);  
-//       })
-// });
+router.post('/', function(req, res, next) {
+  db.MedData.find()
+    .then(function(medData) {
+      medData = medData.reverse();
+      res.json(medData);
+    })
+    .catch(function(err) {
+        res.send(err);
+  })
+});
 
 module.exports = router;
