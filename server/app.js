f8364b4985f01166f13a8a863de3795a3e930740
Aayush Dutt <aayushd100@gmail.com>
2018-10-28 06:05:59 +0530

    remove redundant dependencies

--- server/app.js
+++ server/app.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var medDataRouter = require('./routes/medData');
@@ -9,11 +7,8 @@ var alarmDataRouter = require('./routes/alarmData');
 
 var app = express();
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/medData', medDataRouter);
